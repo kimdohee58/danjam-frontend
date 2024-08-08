446aecd5e7f64e4971ea67a05c057d8928f01e6e
kimdohee58 <kimdohee58@gmail.com>
2024-08-08 16:53:13 +0900

    Header

--- src/search/Search.js
+++ src/search/Search.js
@@ -1,30 +1,6 @@
-import DatePicker from "react-datepicker";
-import {useState} from "react";
-import 'react-datepicker/dist/react-datepicker.css';
-
 function Search() {
-    const [selectedDate, setSelectedDate] = useState(new Date());
-
-    // https://ityranno.tistory.com/entry/react-Spring-Boot-Spring-Boot-%EB%8D%B0%EC%9D%B4%ED%84%B0-react%EC%97%90-%EA%B0%80%EC%A0%B8%EC%98%A4%EA%B8%B0
-
     return (
-        <>
-            <div style={{display: "flex", justifyContent: "space-evenly"}}>
-                <h1>City</h1>
-                <DatePicker
-                    dateFormat='yyyy.MM.dd' // 날짜 형태
-                    shouldCloseOnSelect // 날짜를 선택하면 datepicker가 자동으로 닫힘
-                    minDate={new Date()} // minDate 이전 날짜 선택 불가
-                    maxDate={new Date('2025-12-31')} // maxDate 이후 날짜 선택 불가
-                    selected={selectedDate}
-                    onChange={(date) => setSelectedDate(date)}
-                />
-                <h1>Person</h1>
-            </div>
-            <div>
-
-            </div>
-        </>
+        <h1>Search</h1>
     );
 }
 
